1a8eaea14e76c629d451d56d3acb1c3ed295698b
AdeDevs <adeyemiakinyemi01@gmail.com>
2023-03-17 19:20:06 +0100

    Updated

---
+++ src/deploy.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiV952iXu9wfYJte8_x8LALGV2Gjp5fmE",
+  authDomain: "kryptykaltcounter.firebaseapp.com",
+  projectId: "kryptykaltcounter",
+  storageBucket: "kryptykaltcounter.appspot.com",
+  messagingSenderId: "366825073525",
+  appId: "1:366825073525:web:cd5be881e1a15e5db2013c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
